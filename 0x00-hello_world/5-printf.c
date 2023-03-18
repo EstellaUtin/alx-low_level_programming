b18e90a25de4fd0e5539498de7fca5bee66d5ffe
EstellaUtin <estella.akpabio5@gmail.com>
2023-03-18 21:52:38 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a program that prints followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
