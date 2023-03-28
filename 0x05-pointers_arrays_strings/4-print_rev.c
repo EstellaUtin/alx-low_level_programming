aac6ba5d7075a50eefcea26e86ae0d479695c971
EstellaUtin <estella.akpabio5@gmail.com>
2023-03-28 01:00:22 +0000

    4-printer

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - function that prints in reverse
+ * @s: 1st argument char type
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	};
+	_putchar('\n');
+}
