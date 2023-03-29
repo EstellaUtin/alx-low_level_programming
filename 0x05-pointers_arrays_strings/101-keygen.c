81f6c72ed319b506a851d33911993673e5d3393d
EstellaUtin <estella.akpabio5@gmail.com>
2023-03-29 12:54:02 +0000

    to generate random password for 101Crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,17 +1,54 @@
-#include "main.h"
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
- * main - generate random valid passwords for program
- *
- * Return: (0) random valid passwords
+ * main - Generates random valid passwords for the
+ * program 101-crackme.
+ * 
+ * Return: Always 0.
  */
 int main(void)
 {
-	srand(time(0));
-	int num = rand();
+	char password[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
 
-	printf("%i\n", num);
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		sum += password[index++];
+	}
+	
+	password[index] = '\0';
+	
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+			diff_half1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half1))
+			{
+				password[index] -= diff_half1;
+				break;
+			}																				}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff_half2))
+			{
+				password[index] -= diff_half2;
+				break;
+			}
+		}
+	}
+	
+	printf("%s", password);
+	
 	return (0);
 }
+
