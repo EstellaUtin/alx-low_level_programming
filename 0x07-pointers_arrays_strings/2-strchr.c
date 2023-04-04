c0f951a3f5029b4c16a2b4f161567f63d6791eed
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-04 01:49:55 +0000

    to find charcter c in string s

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strchr - locates character c in string s
+ * @s: to locate in s
+ * @c: character to locate
+ *
+ * Return: character c in string s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+
+	if (*s != c)
+	s++;
+
+	else
+		return (s);
+	}
+	if (c =='\0')
+		return (s);
+
+	return (NULL);
+}
