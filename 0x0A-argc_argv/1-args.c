2be1449aa24479beebee5ea055c5de6215be6625
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-08 17:20:03 +0000

    betty correction and deleting of swp file

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,14 @@
  * main - function to print the number of arguments passed
  * @argc: number of arguments
  * @argv: string of argument in function
- * 
+ *
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
 	(void)*argv;
+
 	printf("%d\n", argc - 1);
 	return (0);
 }
