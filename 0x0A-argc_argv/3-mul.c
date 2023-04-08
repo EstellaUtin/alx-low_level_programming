8f4766effec5e451b294298baaf4467e63de8895
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-08 22:33:26 +0000

    print multiplication of two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply two munbers
+ * @argc: number of arguments
+ * @argv: string vector
+ *
+ * Return: 1.
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc == 3)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+			printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
