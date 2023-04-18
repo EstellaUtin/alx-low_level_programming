0704a2e54bef0dbaf10629114cf981b81b619020
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-18 01:58:23 +0000

    structdog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type def of a struct dog
+ * @name: Char type
+ * @age: float type
+ * @owner: char type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
