04690b4e08331dc9b23f0bc904be2eb2292f6236
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-27 23:52:13 +0000

    1-list_len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Count the elements of a list_t list.
+ * @h: Pointer 2  list.
+ * Return: Number of Elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	
+	}
+	return (elements);
+}
