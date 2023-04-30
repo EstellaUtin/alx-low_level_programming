1ed6680daab29f4f3b35b21b33f7835cc4380de7
EstellaUtin <estella.akpabio5@gmail.com>
2023-04-30 13:04:59 +0000

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - function that adds a new node at the beginning of a listint_t list
+ * @head: pointer to the first node
+ * Return: The sum of all the data n
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
